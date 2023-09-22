4a1ae2c5087ecf7ca1c90bd76abcc0dc882078ea
Nikita Malygin <n.malygin@id-sys.ru>
2023-09-22 14:16:47 +0300

    Parameters container for AbstractOrder

--- sources/ai-trader/ai-commons/src/main/java/aitrader/util/container/AbstractOrder.java
+++ sources/ai-trader/ai-commons/src/main/java/aitrader/util/container/AbstractOrder.java
@@ -1,4 +1,24 @@
 package aitrader.util.container;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class AbstractOrder {
+  protected Map<String, Object> parameters = new HashMap<>();
+
+  public void setParameter(String name, Object value) {
+    parameters.put(name, value);
+  }
+
+  public boolean hasParameter(String name) {
+    return parameters.containsKey(name);
+  }
+
+  public Object getParameter(String name) {
+    return hasParameter(name) ? parameters.get(name) : null;
+  }
+
+  public void absorbParameters(AbstractOrder order) {
+    parameters.putAll(order.parameters);
+  }
 }
